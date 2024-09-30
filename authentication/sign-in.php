11c6f16724250bec92f447811afd3fb2b6f9bd92
Sajad Pakravan <pakravan1118sa@gmail.com>
2024-09-30 14:55:09 +0330

    403.07.09

--- authentication/sign-in.php
+++ authentication/sign-in.php
@@ -2,39 +2,30 @@
 global $pdo;
 include '../tools/db_connect.php';
 
-if (postMethod()) {
-    $username = $_POST['username'] ?? '';
-    $password = $_POST['password'] ?? '';
+postMethod();
 
-    if (empty($username)) {
-        http_response_code(400);
-        $response = ['status' => 'username Null', 'message' => 'نام کاربری وارد نشده است'];
-        exit(json_encode($response));
-    }
+$username = $_POST['username'] ?? '';
+$password = $_POST['password'] ?? '';
 
-    if (empty($password)) {
-        http_response_code(400);
-        $response = ['status' => 'password Null', 'message' => 'گذرواژه وارد نشده است'];
-        exit(json_encode($response));
-    }
+nullCheck($username, 'username');
+nullCheck($password, 'password');
 
-    $checkUsername = $pdo->prepare('SELECT * FROM users_auth WHERE username = ?');
-    $checkUsername->execute([$username]);
-    $auth = $checkUsername->fetch();
+$checkUsername = $pdo->prepare('SELECT * FROM users_auth WHERE username = ?');
+$checkUsername->execute([$username]);
+$auth = $checkUsername->fetch();
 
-    if ($auth) {
-        if (password_verify($password, $auth['password'])) {
-            $updateAuth = $pdo->prepare('UPDATE users_auth SET status = 1, login_time = NOW() WHERE username = ?');
-            $updateAuth->execute([$username]);
-            createToken($auth['user_id']);
-        } else {
-            http_response_code(400);
-            $response = ['status' => 'Invalid Inputs', 'message' => 'نام کاربری یا گذرواژه صحیح نیست'];
-            exit(json_encode($response));
-        }
+if ($auth) {
+    if (password_verify($password, $auth['password'])) {
+        $updateAuth = $pdo->prepare('UPDATE users_auth SET status = 1, login_time = NOW() WHERE username = ?');
+        $updateAuth->execute([$username]);
+        createToken($auth['user_id']);
     } else {
         http_response_code(400);
         $response = ['status' => 'Invalid Inputs', 'message' => 'نام کاربری یا گذرواژه صحیح نیست'];
         exit(json_encode($response));
     }
+} else {
+    http_response_code(400);
+    $response = ['status' => 'Invalid Inputs', 'message' => 'نام کاربری یا گذرواژه صحیح نیست'];
+    exit(json_encode($response));
 }
--- authentication/sign-up.php
+++ authentication/sign-up.php
@@ -2,46 +2,31 @@
 global $pdo;
 include '../tools/db_connect.php';
 
-if (postMethod()) {
-    $username = $_POST['username'] ?? '';
-    $name = $_POST['name'] ?? '';
-    $password = $_POST['password'] ?? '';
+postMethod();
 
-    if (empty($username)) {
-        http_response_code(400);
-        $response = ['status' => 'username Null', 'message' => 'نام کاربری وارد نشده است'];
-        exit(json_encode($response));
-    }
+$username = $_POST['username'] ?? '';
+$name = $_POST['name'] ?? '';
+$password = $_POST['password'] ?? '';
 
-    if (empty($name)) {
-        http_response_code(400);
-        $response = ['status' => 'name Null', 'message' => 'نام وارد نشده است'];
-        exit(json_encode($response));
-    }
+nullCheck($username, 'username');
+nullCheck($name, 'name');
+nullCheck($password, 'password');
 
-    if (empty($password)) {
-        http_response_code(400);
-        $response = ['status' => 'password Null', 'message' => 'گذرواژه وارد نشده است'];
-        exit(json_encode($response));
-    }
+$checkUsername = $pdo->prepare('SELECT * FROM users_auth WHERE username = ?');
+$checkUsername->execute([$username]);
 
-    $checkUsername = $pdo->prepare('SELECT * FROM users_auth WHERE username = ?');
-    $checkUsername->execute([$username]);
+if ($checkUsername->fetch()) {
+    exit(json_encode(['status' => 'Username Used', 'message' => 'این نام کاربری قبلا استفاده شده']));
+} else {
+    $insertUser = $pdo->prepare('INSERT INTO users (name, email, phone, avatar, create_at) VALUES (?, null, null, ?, NOW())');
+    if ($insertUser->execute([$name, DefaultAvatarUrl])) {
+        $insertAuth = $pdo->prepare('INSERT INTO users_auth (user_id, username, password, login_time, Logout_time, status) VALUES (?, ?, ?, NOW(), null, 1)');
 
-    if ($checkUsername->fetch()) {
-        $response = ['status' => 'Username Used', 'message' => 'این نام کاربری قبلا استفاده شده'];
-        exit(json_encode($response));
-    } else {
-        $insertUser = $pdo->prepare('INSERT INTO users (name, email, phone, avatar, create_at) VALUES (?, null, null, ?, NOW())');
-        if ($insertUser->execute([$name, DefaultAvatarUrl])) {
-            $insertAuth = $pdo->prepare('INSERT INTO users_auth (user_id, username, password, login_time, Logout_time, status) VALUES (?, ?, ?, NOW(), null, 1)');
+        $userId = $pdo->lastInsertId();
+        $hashPass = password_hash($password, PASSWORD_DEFAULT);
 
-            $userId = $pdo->lastInsertId();
-            $hashPass = password_hash($password, PASSWORD_DEFAULT);
+        $insertAuth->execute([$userId, $username, $hashPass]);
 
-            $insertAuth->execute([$userId, $username, $hashPass]);
-
-            createToken($userId);
-        }
+        createToken($userId);
     }
 }
