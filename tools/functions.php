2903bbec279cac06679fbe731490d3131494764a
Sajad Pakravan <pakravan1118sa@gmail.com>
2024-10-05 00:53:51 +0330

    403.07.14

--- tools/functions.php
+++ tools/functions.php
@@ -8,12 +8,10 @@ require 'PHPMailer/src/Exception.php';
 require 'PHPMailer/src/PHPMailer.php';
 require 'PHPMailer/src/SMTP.php';
 
-if (!function_exists('setError')) {
-    function setError(int $code, string $error): void
-    {
-        http_response_code($code);
-        exit(json_encode(['error' => $error]));
-    }
+function setError(int $code, string $error): void
+{
+    http_response_code($code);
+    exit(json_encode(['error' => $error]));
 }
 
 function setMethod(string $method): void
