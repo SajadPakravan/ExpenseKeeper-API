7c98c72180da012c5495b5c24d6d56233eb4239d
Sajad Pakravan <pakravan1118sa@gmail.com>
2024-10-05 02:06:49 +0330

    403.07.14

--- authentication/sign-in.php
+++ authentication/sign-in.php
@@ -7,9 +7,9 @@ $password = param('password');
 
 $auth = Database::select(table: 'users_auth', where: 'username = ?', value: [$username]);
 if (!empty($auth)) {
-    if (password_verify($password, $auth['password'])) {
+    if (password_verify($password, $auth[0]['password'])) {
         Database::update(table: 'users_auth', set: ['status' => 1, 'login_time' => date('Y-m-d H:i:s')], where: ['username' => $username]);
-        $token = createToken($auth['user_id']);
+        $token = createToken($auth[0]['user_id']);
         exit(json_encode(['message' => 'ورود شما با موفقیت انجام شد', 'token' => $token]));
     }
 }
