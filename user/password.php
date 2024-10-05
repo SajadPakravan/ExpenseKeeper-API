bcac539feb5fc5d407578fb211d13c69ecc41147
Sajad Pakravan <pakravan1118sa@gmail.com>
2024-10-05 16:46:29 +0330

    403.07.14

--- user/password.php
+++ user/password.php
@@ -6,9 +6,9 @@ $id = authorization();
 $currentPass = param('current-password');
 $newPass = param('new-password');
 
-$user = Database::select(table: 'users_auth', where: 'user_id = ?', value: [$id]);
+$auth = Database::select(table: 'users_auth', where: 'user_id = ?', value: [$id]);
+if (!(password_verify($currentPass, $auth[0]['password']))) setError(400, 'Incorrect Current Password');
 
-if (!(password_verify($currentPass, $user[0]['password']))) setError(400, 'Incorrect Current Password');
 $hashPass = password_hash($newPass, PASSWORD_DEFAULT);
-Database::update(table: 'users_auth', set: ['password' => $hashPass], where: ['id' => $id]);
+Database::update(table: 'users_auth', set: ['password' => $hashPass], where: ['user_id' => $id]);
 exit(json_encode(['message' => 'گذرواژه شما با موفقیت تغییر کرد']));
